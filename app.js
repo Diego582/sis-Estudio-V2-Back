ca15bc69dab8ec5f2f820e95c045a9eef35ed31e
Diego <penaomardiego@gmail.com>
2023-10-16 20:31:40 -0300

    firstcommit

---
+++ app.js
@@ -0,0 +1,39 @@
+import "dotenv/config.js";
+import __dirname from "./utils.js";
+//var createError = require('http-errors');
+import createError from "http-errors";
+import express from "express";
+//var express = require('express');
+import path from "path";
+//var path = require('path');
+//var cookieParser = require('cookie-parser');
+//var logger = require('morgan');
+import logger from "morgan";
+import indexRouter from "./routes/index.js";
+import notFoundHandler from "./middlewares/notFoundHandler.js";
+import errorHandler from "./middlewares/errorHandler.js";
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+import cors from "cors";
+
+let app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(notFoundHandler);
+
+// error handler
+app.use(errorHandler);
+
+export default app;
---
+++ routes/users.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+import read from "../controllers/users/read.js";
+
+const router = express.Router();
+
+/* GET users listing. */
+
+router.get("/", read);
+
+export default router;
